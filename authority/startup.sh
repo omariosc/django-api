d0f8174f0506c9369244207805ae2ebe4a25063d
Omar Choudhry <sc20osc@leeds.ac.uk>
2023-05-11 01:02:16 +0100

    Updated CI and reqs

--- authority/startup.sh
+++ authority/startup.sh
@@ -2,7 +2,7 @@
 
 python manage.py makemigrations api
 python manage.py migrate
+python manage.py runtests
 python manage.py populate_database
 python manage.py create_admin
-python manage.py runtests
 python manage.py runserver
